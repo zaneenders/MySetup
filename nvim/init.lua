2857e39f55c1bf1c8e0d08948ef2f324eaa524b5
zane <39070793+zaneenders@users.noreply.github.com>
2024-01-09 22:15:16 -0700

    config changes, alacritty update

--- nvim/init.lua
+++ nvim/init.lua
@@ -592,7 +592,7 @@ end, 0)
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'racket', 'swift' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'racket', 'swift', 'r' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
